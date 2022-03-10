a04a5b5bb1d7d53e5fc9d8fee0253fcdc64e498d
Evan Irving-Pease <evan.irvingpease@gmail.com>
2022-03-10 14:52:49 +0000

    added setup script for PALM

---
+++ setup_palm.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Author:    Evan K. Irving-Pease
+# Copyright: Copyright 2022, University of Copenhagen
+# Email:     evan.irvingpease@gmail.com
+# License:   MIT
+
+# fetch the PALM code
+git clone git@github.com:35ajstern/palm.git --branch master bin/palm/
