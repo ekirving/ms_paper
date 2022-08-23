b49e757946e88b46f0f3451243699d9f220f068d
Evan Irving-Pease <evan.irvingpease@gmail.com>
2022-08-23 09:00:58 +0100

    only retain UKBB associations for SNPs used in the PALM analysis

--- scripts/filter_ukbb_gwas.R
+++ scripts/filter_ukbb_gwas.R
@@ -12,9 +12,9 @@ quiet(library(argparser)) # v0.6
 quiet(library(tidyverse)) # v1.3.1
 
 # get the command line arguments
-p <- arg_parser("Filter the UKBB GWAS file to retain only the SNPs with a significant selection test in the CLUES analysis")
+p <- arg_parser("Filter the UKBB GWAS file to retain only the SNPs with a significant GWAS assocation in the focal trait")
 p <- add_argument(p, "--pheno", help = "UKBB phenotype", default = "C21")
-p <- add_argument(p, "--ukbb", help = "UKBB assocations", default = "data/ukbb/nealelab/gwas/C21.gwas.imputed_v3.both_sexes.significant.tsv.bgz")
+p <- add_argument(p, "--ukbb", help = "UKBB associations", default = "data/ukbb/nealelab/gwas/C21.gwas.imputed_v3.both_sexes.significant.tsv.bgz")
 p <- add_argument(p, "--palm", help = "PALM report for all ancestries", default = "results/palm/ancestral_paths_new-ms-all-palm_report_prs.tsv")
 p <- add_argument(p, "--output", help = "Output file", default = "results/palm/ukbb/ancestral_paths_new-ms-all.C21.gwas.imputed_v3.both_sexes.significant.tsv")
 
@@ -23,17 +23,16 @@ argv <- parse_args(p)
 ukbb <- read_tsv(argv$ukbb, col_types = cols())
 palm <- read_tsv(argv$palm, col_types = cols())
 
-# get the list of SNPs with marginally significant p-values in CLUES
-selected_snps <- palm %>%
-    filter(significant == TRUE) %>%
+# get the list of SNPs used in the PALM analysis
+snps <- palm %>%
     # UKBB uses `{chr}:{pos}:{ref}:{alt}` as the variant ID
     mutate(variant = paste(chrom, pos, ref, alt, sep = ":")) %>%
     pull(variant) %>%
     unique()
 
-# get the UKBB associations for the significant SNPs only
+# only retain UKBB associations for SNPs used in the PALM analysis
 ukbb <- ukbb %>%
-    filter(variant %in% selected_snps) %>%
+    filter(variant %in% snps) %>%
     mutate(phenotype = argv$pheno, .before = 1)
 
 write_tsv(ukbb, argv$output)
